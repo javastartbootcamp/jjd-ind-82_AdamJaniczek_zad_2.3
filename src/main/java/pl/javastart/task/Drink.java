8cceaa1384653e3badf4ee329dede1408d63bfc3
AdamJaniczek <152891309+AdamJaniczek@users.noreply.github.com>
2023-12-06 19:37:57 +0100

    Utworzenie zadania

---
+++ src/main/java/pl/javastart/task/Drink.java
@@ -0,0 +1,10 @@
+package pl.javastart.task;
+public class Drink {
+    String name;
+    double price;
+    boolean containsAlcohol;
+    Ingredient firstIngredient = new Ingredient();
+    Ingredient secondIngredient = new Ingredient();
+    Ingredient thirdIngredient = new Ingredient();
+
+}
